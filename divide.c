40b46317d9140a7e706d623c58a8241f6df37458
Hemlata-joshi <155806465+Hemlata-joshi@users.noreply.github.com>
2024-01-08 10:52:26 +0545

    Add files via upload

---
+++ divide.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+system("cls");
+int a,b;
+printf("enter any two number\n");
+scanf("%d %d",&a ,&b);
+printf("a is divided by b=%d\n",a/b);
+ return 0;
+ }
+ 
+ 
+
+
+    
