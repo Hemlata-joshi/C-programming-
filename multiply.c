9aa6e6ff2accbdd648f6097a9e2f6abbd8561153
Hemlata-joshi <155806465+Hemlata-joshi@users.noreply.github.com>
2024-01-08 10:51:53 +0545

    Add files via upload

---
+++ multiply.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+system("cls");
+int a,b;
+printf("enter any two number\n");
+scanf("%d %d",&a ,&b);
+printf("a multiply with b=%d\n",a*b);
+ return 0;
+ }
+ 
+ 
+
+
+    
