effbdad9fead931862ea076ed4b6ee4b93d1702d
Hemlata-joshi <155806465+Hemlata-joshi@users.noreply.github.com>
2024-01-08 14:49:12 +0545

    Add files via upload

---
+++ rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+system("cls");
+int l,b;
+float area;
+printf("enter length of rectangle\n");
+scanf("%d",&l);
+printf("enter breadth of rectangle\n");
+scanf("%d",&b);
+printf("area of rectangle=%d\n",l*b);
+return 0;
+}
