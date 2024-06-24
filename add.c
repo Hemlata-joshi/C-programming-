2f104312b663f8b1f89475d4c4ef21ffad374da8
Hemlata-joshi <155806465+Hemlata-joshi@users.noreply.github.com>
2024-06-24 18:37:27 +0545

    Add files via upload

---
+++ add.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int add(int,int);
+int main()
+{
+    int a,b,c;
+    printf("enter two numbers");
+    scanf("%d %d,&a,&b");
+    c=add(a,b);
+    printf("sum=%d\n",c);
+    return 0;
+
+}
+int add(intp,intq)
+{
+    int p,q;
+    int t;
+    t=p+q;
+    return(t);
+    
+}
